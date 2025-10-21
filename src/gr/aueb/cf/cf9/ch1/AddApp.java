45c4b4347ac1a74fe7ed172a92e0dcb31016c55b
a8anassis <a8anassis@gmail.com>
2025-10-21 19:21:28 +0300

    feat: initial commit

---
+++ src/gr/aueb/cf/cf9/ch1/AddApp.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.cf9.ch1;
+
+/**
+ *  Προσθέτει δύο ακεραίους και εμφανίζει το
+ *  αποτέλεσμα στην κονσόλα.
+ */
+public class AddApp {
+
+    public static void main(String[] args) {
+
+        // Δήλωση και αρχικοποίηση μεταβλητών
+        int num1 = 5;
+        int num2 = 10;
+        int result;
+
+        // Εντολές
+        result = num1 + num2;
+
+        // Εκτύπωση αποτελέσματος
+        System.out.println("Το αποτέλεσμα είναι: " + result);
+    }
+}
