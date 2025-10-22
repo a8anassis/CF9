c3a092b089b07b48e5b2d1ba911d9c409caa6e3d
a8anassis <a8anassis@gmail.com>
2025-10-22 19:31:37 +0300

    fix: add different forms of output with println and printf

--- src/gr/aueb/cf/cf9/ch2/TypeCastApp.java
+++ src/gr/aueb/cf/cf9/ch2/TypeCastApp.java
@@ -5,7 +5,11 @@ public class TypeCastApp {
     public static void main(String[] args) {
         long myLong = (long) 2_147_483_647;
         int myInt = (int) myLong;
+
+        System.out.println("Hello World!");
+        System.out.println(myLong);
         System.out.println("My long is: " + myLong);
-        System.out.println("My int is: " + myInt);
+        System.out.printf("My long is: %d%n", myLong);
+
     }
 }
